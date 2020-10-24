11c8b94ae9a59986b5f632e804445dd7b103407c
Bohdan <dogdanlashko@gmail.com>
2020-10-24 19:21:56 +0300

    🍗

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 conn = sqlite3.connect('quize.db')
 c = conn.cursor()
 
+c.execute("""
+    CREATE TABLE IF NOT EXISTS questions(
+        q TEXT,
+        a1 TEXT,
+        a2 TEXT,
+        a3 TEXT
+    )
+    CREATE TABLE IF NOT EXISTS answ(
+        a INTENGER
+    )
+""")
+conn.commit()
 
 @app.route("/<n>", methods=['GET', 'POST'])
 def main(n):
