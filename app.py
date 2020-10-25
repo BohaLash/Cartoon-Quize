b7846da10f232d3d1dab04ba15ce79b0bedc5d98
Bohdan <dogdanlashko@gmail.com>
2020-10-25 09:49:18 +0200

    🥩

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, redirect
 import sqlite3
+import random
 
 app = Flask(__name__)
 
@@ -13,17 +14,50 @@ c.execute("""
         a2 TEXT,
         a3 TEXT
     )
+""")
+conn.commit()
+
+c.execute("""
     CREATE TABLE IF NOT EXISTS answ(
         a INTENGER
     )
 """)
 conn.commit()
 
-@app.route("/<n>", methods=['GET', 'POST'])
-def main(n):
-    q = 'hello'
-    a = ['a', 'b', 'c']
-    return render_template('index.html', question=q, answ = a)
+
+global a, q
+a = {0: 0}
+q = {0: 0}
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    global a, q
+    if request.method == "POST":
+        key = random.randrange(99999)
+        while key in a.keys() or key in q.keys():
+            key = random.randrange(99999)
+        a[key] = 0
+        q[key] = 0
+        return redirect('/q/' + str(key))
+    return render_template("index.html")
+
+
+@app.route('/q/<n>', methods=["GET", "POST"])
+def question(n):
+    global a, q
+    if request.method == "POST":
+        answ = request.form['answ']
+        a[n] += answ
+        q[n] += 1
+        return redirect('/q/' + str(n))
+    with sqlite3.connect("quize.db") as con:
+        cur = con.cursor()
+        row = cur.execute(
+            "SELECT * FROM question WHERE rowid = 1")
+    question = row[0]
+    answs = [row[1], row[2], row[3]]
+    return render_template("question.html", question=question, answ=answs)
 
 
 if __name__ == "__main__":
