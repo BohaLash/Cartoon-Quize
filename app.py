566251545d4a44b3fa1ad90ef2773efef884653d
Bohdan <dogdanlashko@gmail.com>
2020-11-07 13:05:27 +0200

    🦷

--- app.py
+++ app.py
@@ -27,14 +27,15 @@ c = conn.cursor()
 # conn.commit()
 
 
-global a, q
+global a, q, m
 a = {}
 q = {}
+m = {}
 
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    global a, q
+    global a, q, m
     # starting quize
     if request.method == "POST":
         # generate user hash
@@ -43,15 +44,16 @@ def main():
             key = random.randrange(99999)
         a[key] = 0
         q[key] = 1
+        m[key] = 'Good Luck!'
         return redirect('/q/' + str(key))
     return render_template("index.html")
 
 
 @app.route('/q/<n>', methods=["GET", "POST"])
 def question(n):
-    global a, q
+    global a, q, m
     # on invalid user hesh
-    if not (int(n) in a.keys() or int(n) in q.keys()):
+    if not (int(n) in a.keys() or int(n) in q.keys() or int(n) in m.keys()):
         return redirect('/')
     # on resiving answer
     if request.method == "POST":
@@ -62,8 +64,19 @@ def question(n):
             data = cur.execute(
                 "SELECT * FROM answ WHERE rowid = ?", (q[int(n)], ))
             t = int(data.fetchone()[0])
+        # if answ is true
         if answ == t:
             a[int(n)] += 1
+            m[int(n)] = 'Good job!'
+        else:
+            # get right answ from db
+            with sqlite3.connect("quiz.db") as con:
+                cur = con.cursor()
+                data = cur.execute(
+                    "SELECT * FROM questions WHERE rowid = ?", (q[int(n)], ))
+                row = data.fetchone()
+                # put it in messaage
+                m[int(n)] = 'Your made a mistake! The correct answer was: ' + str(row[t])
         q[int(n)] += 1
         return redirect(f'/q/{str(n)}' if q[int(n)] <= 48 else f'/res/{str(n)}')
     # get question and answers
@@ -74,18 +87,19 @@ def question(n):
         row = data.fetchone()
         question = str(row[0])
         answs = [str(row[1]), str(row[2]), str(row[3]), str(row[4])]
-    return render_template("question.html", question=question, answ=answs)
+    return render_template("question.html", question=question, answ=answs, res=a[int(n)], message=m[int(n)])
 
 
 @app.route('/res/<n>', methods=["GET", "POST"])
 def result(n):
-    global a, q
+    global a, q, m
     # on invalid user hesh or button click
-    if not (int(n) in a.keys() or int(n) in q.keys()) or request.method == "POST":
+    if not (int(n) in a.keys() or int(n) in q.keys() or int(n) in m.keys()) or request.method == "POST":
         return redirect('/')
     q.pop(int(n))
+    m.pop(int(n))
     return render_template("result.html", res=a.pop(int(n)))
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=True)
